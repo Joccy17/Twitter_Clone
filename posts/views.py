62c6e4ee34816ad0d9594d384adcd3f2367cd8c3
Joyce Olufunso <joyce@Joyces-Air.lan>
2022-08-08 12:25:53 -0500

    update

---
+++ posts/views.py
@@ -0,0 +1,62 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Post
+from .forms import PostForm
+from django import forms
+from django.forms.fields import ImageField
+
+from .forms import PostForm, PictureForm
+
+def index(request):
+    #If the method is POST
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        #If the form is valid
+        if form.is_valid():
+            # Yes, Save
+            form.save()
+
+            #Redirect to Home
+            return HttpResponseRedirect('/')
+
+        else:
+            # No Show Error
+            return HttpResponseRedirect(form.erros.as_json())
+
+        # Get all posts, limit = 20
+    posts = Post.objects.all().order_by('-created_at')[:20]
+
+    form = PostForm()
+
+        # Show
+    return render(request, 'posts.html',
+                        {'posts': posts})
+
+def delete(request, post_id):
+    # Find post
+    post = Post.objects.get(id = post_id)
+    post.delete()
+    return HttpResponseRedirect('/')
+
+def edit(request, post_id):
+    # Find post
+    post = Post.objects.get(id = post_id)
+    if request.method == "POST":
+        form = PostForm(request.POST,request.FILES, instance = post)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect("not valid")
+
+    form = PostForm
+
+    return render(request, 'edit.html', {'post': post, 'form': form})
+
+
+def like(request, post_id):
+    newlikecount =Post.objects.get(id=post_id)
+    newlikecount.likecount += 1
+    newlikecount.save()
+    return HttpResponseRedirect('/')
---
+++ static/js/posts.js
@@ -0,0 +1,13 @@
+//////////////////////////////
+///// JavaScript for Posts page
+////////////////////////////////
+
+
+
+
+$(function() {
+    // Executed when js-menu-icon js clicked
+    $('.js-menu-icon').click(function() {
+        $(this).next().toggle();
+    })
+})
